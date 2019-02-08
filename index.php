9dcf8c73afec61fc38db4d2dd2f41115ea328bdb
Noor <nalam@svsu.edu>
2019-02-07 23:11:15 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8" />
+		<link   href="css/bootstrap.min.css" rel="stylesheet" />
+		<script src="js/bootstrap.min.js"></script>
+	</head>
+
+	<body>
+		<div class="container">
+			<div class="row">
+      <a href="" class="btn btn-success">Github Link</a>
+				<h3>PHP CRUD Grid</h3>
+			</div>
+			<div class="row">
+				<p>
+					<a href="create.php" class="btn btn-success">Create</a>
+				</p>
+
+				<table class="table table-striped table-bordered">
+					<thead>
+						<tr>
+							<th>Name</th>
+							<th>Email Address</th>
+							<th>Mobile Number</th>
+							<th>Action</th>
+						</tr>
+					</thead>
+					<tbody>
+						<?php 
+						    require 'database.php';
+						    $pdo = Database::connect();
+						    $sql = 'SELECT * FROM customers ORDER BY id DESC';
+						    foreach ($pdo->query($sql) as $row) {
+								echo '<tr>';
+								echo '<td>'. $row['name'] . '</td>';
+								echo '<td>'. $row['email'] . '</td>';
+								echo '<td>'. $row['mobile'] . '</td>';
+								echo '<td width=250>';
+								echo '<a class="btn" href="read.php?id='.$row['id'].'">Read</a>';
+								echo '&nbsp;';
+								echo '<a class="btn btn-success" href="update.php?id='.$row['id'].'">Update</a>';
+								echo '&nbsp;';
+								echo '<a class="btn btn-danger" href="delete.php?id='.$row['id'].'">Delete</a>';
+								echo '</td>';
+								echo '</tr>';
+						    }
+						    Database::disconnect();
+						?>
+					</tbody>
+				</table>
+			</div>
+			<footer>
+				For source code, see: <a href="https://www.startutorial.com/articles/view/php-crud-tutorial-part-1">Star Tutorial</a>
+			</footer>
+		</div> <!-- /container -->
+
+	</body>
+</html>
